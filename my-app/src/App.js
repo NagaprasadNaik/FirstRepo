61a47fc7e76e6373f94f9b8bfe3872f462174be9
Nagaprasad Naik <naiknaga99@gmail.com>
2022-11-15 23:28:31 -0800

    16-11-2022 ReactJS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import Button from 'react-bootstrap/Button'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 // function App(){
 //   return React.createElement("h1",{},"Hello world!!");
 // }
@@ -30,12 +33,90 @@ import React from 'react';
 //   );
 // }
 
+// function App () {
+//   return (
+//   <>
+//   <h1>Hello World !!! </h1>
+//   <img src={logo} width="120" height="120"  />
+//   <p>ReactJS for UI</p>
+//   </>
+//   );
+// }
+
+
+
+// function App() {
+//   let list = [
+//     { eId: 123, eName: 'Prasad', eQuali: 'Diploma' },
+//     { eId: 456, eName: 'Sharanu', eQuali: 'Diploma' },
+//     { eId: 789, eName: 'Vivek', eQuali: 'Diploma' },
+//   ];
+//   return (
+//     <>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>EMP ID</th>
+//             <th>EMP NAME</th>
+//             <th>EMP OUALIFICATION</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {list.map((emp) => {
+//             return (
+//               <tr key = {emp.eId}>
+//                 <td>{emp.eId}</td>
+//                 <td>{emp.eName}</td>
+//                 <td>{emp.eQuali}</td>
+//               </tr>
+//             )
+//           })
+//           }
+//         </tbody>
+//       </table>
+//     </>
+//   );
+// }
+
+
+// function App () {
+//   return (
+//   <>
+//   <h1 style={{color:'red'}}>Hello World !!! </h1>
+//   </>
+//   );
+// }
+
+// function App () {
+//   return (
+//   <>
+//   <h1 style={{color:'red', backgroundColor:'black'}}>Hello World !!! </h1>
+//   </>
+//   );
+// }
+
+
+// function App () {
+//   return (
+//   <>
+//   <button className="btn btn-danger">submit</button>
+//   </>
+//   );
+// }
+
+// function App () {
+//   return (
+//   <>
+//   <Button variant="contained" color="primary">click here</Button>
+//   <Typography variant="h4" gutterBottom>Welcome to React</Typography>
+//   </>
+//   );
+// }
+
 function App () {
   return (
   <>
-  <h1>Hello World !!! </h1>
-  <img src={logo} width="120" height="120" />
-  <p>ReactJS for UI</p>
+  <button className="btn btn-danger">submit</button>
   </>
   );
 }
